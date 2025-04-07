5769a0db3c957a194bcb34d94dc7bb8b5da7bfad
DamianoSJA <damiano@sja.com>
2025-04-07 12:39:11 +0200

    mod02

--- javascript/02a_arr_over/arr_over.js
+++ javascript/02a_arr_over/arr_over.js
@@ -1,7 +1,8 @@
 var arr = [1, 4, 10, 75, 30, 60];
 
+//stampa una tabella con index e values
 console.table(arr);
 
-// for (var i = 0; i < 10; i++) {
-//     console.log("array: " + arr[i]);
-// }
+for (var i = 0; i < 10; i++) {
+    console.log("array: " + arr[i]);
+}
