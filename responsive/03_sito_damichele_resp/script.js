3fc05461c24c1b9b20542cf8772bea65e54778ed
DamianoSJA <damiano@sja.com>
2025-04-28 13:14:55 +0200

    lezione oggi

---
+++ responsive/03_sito_damichele_resp/script.js
@@ -0,0 +1,26 @@
+var menu =
+[
+    {
+        nome: "Margherita",
+        ingredienti: "x, y, z",
+    },
+    {
+        nome: "Diavola",
+        ingredienti: "x, y, z",
+    },
+    {
+        nome: "3",
+        ingredienti: "x, y, z",
+    },
+    {
+        nome: "4",
+        ingredienti: "x, y, z",
+    },
+    {
+        nome: "5",
+        ingredienti: "x, y, z",
+    }
+    
+]
+
+console.log(menu);
